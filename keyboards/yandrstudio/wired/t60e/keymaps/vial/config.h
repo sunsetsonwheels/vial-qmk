f456d0481846fef770c776f002ee6c28b93ca7e3
sunsetsonwheels <hey@sunsetsonwheels.me>
2025-03-16 09:59:46 +1100

    Port t60e to VIAL
    Modified from upstream, keyboard not yet added to main qmk_firmware repo.

---
+++ keyboards/yandrstudio/wired/t60e/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDF, 0x20, 0x2B, 0x1D, 0x3F, 0x07, 0xC6, 0xD3}
+
+// Esc + Enter
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
