f389eabf9630e468cc10e2c2a117dbf0c9c90219
sunsetsonwheels <hey@sunsetsonwheels.me>
2025-08-03 03:15:59 +1000

    Add Neo60Cu support

---
+++ keyboards/neo/neo60/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x01, 0x40, 0x8D, 0xBC, 0x8C, 0xC7, 0x37, 0x2C}
+
+// Esc + Enter
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
